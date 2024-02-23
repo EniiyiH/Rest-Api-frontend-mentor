14a4775f02474916f5dd7677bcb07b7dcb19490a
Eniiyi <eniiyiheritage@gmail.com>
2024-02-23 18:25:02 +0100

    New page created , functionalities updated.
    Left with the border countries, and accessing each individual objects

---
+++ country.js
@@ -0,0 +1,46 @@
+const details = new URLSearchParams(window.location.search).get('name')
+const container =document.querySelector("#container")
+
+console.log(details)
+function createCard(data){
+    const countryDetails = document.createElement('div');
+    countryDetails.classList.add('country-details');
+    countryDetails.innerHTML=
+    `
+    <img src="${data[0].flags.svg}" alt="${data[0].name.common}">
+    <div class="country-detail">
+        <div class="country-name">
+            <h1>${data[0].name.common}</h1>
+        </div>
+        <div class="country-detail-text">
+            <p><b>Native Name: </b>${data[0].name.nativeName.eng?.common}</p>
+            <p><b>Population: </b>${data[0].population}</p>
+            <p><b>Region: </b>${data[0].region}</p>
+            <p><b>Sub-Region: </b>${data[0].subregion}</p>
+            <p><b>Capital: </b>${data[0].capital}</p>
+            <p><b>Top Level Domain: </b>${data[0].tld}</p>
+            <p><b>Currencies: </b>${JSON.stringify(data[0].currencies)}</p>
+            <p><b>Languages: </b>${JSON.stringify(data[0].languages)}</p>
+        </div>
+        <div class="border-countries">
+            <h3>Border Countries:</h3>
+            <a href="#"> <button>France</button></a>
+            <a href="#"> <button>Germany</button></a>
+            <a href="#"> <button>Netherlands</button></a>
+        </div>
+    </div>
+    `
+    container.appendChild(countryDetails)
+    console.log(container);
+}
+
+
+window.addEventListener("load",()=>{
+fetch(`https://restcountries.com/v3.1/name/${details}`)
+.then(res=>res.json())
+.then(data=>{
+    console.log(data)
+    createCard(data);
+})
+})
+
--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const url = " https://restcountries.com/v3.1/all"
 const form = document.getElementById("inp-word")
 const card = document.querySelector('.card')
+const select = document.querySelector("#filter")
+
 
 
 window.addEventListener("load", function getAllCountries(){
@@ -9,6 +11,7 @@ window.addEventListener("load", function getAllCountries(){
     .then((data)=>{
         data.forEach((item) =>{
             showCountry(item);
+            
         })
     }).catch(error=>{
         Error,error})
@@ -20,6 +23,7 @@ function showCountry(country){
     const cardLink = document.createElement('a');
         cardLink.classList.add('country');
         cardLink.href =`/country.html?name=${country.name.common}`
+        
         cardLink.innerHTML =     
         `
         <div class= "cardItem">
@@ -45,36 +49,91 @@ form.addEventListener("submit",(e)=>{
     fetch(`https://restcountries.com/v3.1/name/${searchValue}?fullText=true`)
     .then(response=> response.json())
     .then(data=>{
-
-
         console.log(data)
-        const cardLink = document.createElement('a');
-        cardLink.classList.add('country');
-        cardLink.href ="/country.html"
-        cardLink.innerHTML =     
-        `
-        <div class= "cardItem">
-            <div class="image-container">
-             <img src="${data.flags.svg}" alt="${data.flags.alt}">       
+        if(data.status!=404){
+            clearCard(card);
+        
+            const cardLink = document.createElement('a');
+            cardLink.classList.add('country');
+            cardLink.href = `/country.html?name=${data[0].name.common}`
+            cardLink.innerHTML =     
+            `
+            <div class= "cardItem">
+                <div class="image-container">
+                 <img src="${data[0].flags.png}" alt="${data[0].flags.alt}">       
+                </div>
+                <div class="card-details">
+                    <h1> ${data[0].name.common}</h1>
+                    <p class="population"><strong>Population:</strong> ${data[0].population}</p>
+                    <p class="region"><strong>Region: </strong>${data[0].region}</p>
+                    <p class="capital"><strong>Capital:</strong>${data[0].capital}</p>         
+                </div>
             </div>
-            <div class="card-details">
-                <h1> ${data.name.official}</h1>
-                <p class="population"><strong>Population:</strong> ${data.population}</p>
-                <p class="region"><strong>Region: </strong>${data.region}</p>
-                <p class="capital"><strong>Capital:</strong>${data.capital}</p>         
-            </div>
-        </div>
-         
-        `
-        card.appendChild(cardLink)
+             
+            `
+            card.appendChild(cardLink)
 
-        console.log(data)
-    })
+        }
+        else{
+            clearCard(card);
+        
+            const cardLink = document.createElement('div');
+            cardLink.classList.add('error');
+            cardLink.innerHTML =     
+            `
+            <div class= "error-item">
+                <div class="error-image">
+                <img src="/5203299.jpg" alt="Not found">  
+                </div>
+                <div class="error-details">
+                    <h1>Not found</h1>       
+                </div>
+            </div>
+             
+            `
+            card.appendChild(cardLink)
+            console.log(data)
+        }
+    }).catch(error=>{
+        Error,error})
 })
 
 
-// form.addEventListener("click",()=>{
-//     document.getElementById("card").classList.toggle("show")
+
+function filter() {
+    var selectedValue = select.value;
+    clearCard(card);
+
+    if(selectedValue != "All"){
+        fetch(`https://restcountries.com/v3.1/region/${selectedValue}`)
+        .then(response=>response.json())
+        .then(data=>{
+            data.forEach((item) =>{
+                showCountry(item);
+            })
+            
+            
+        }).catch(error=>{
+            Error,error})
+    }
+    else{
+        fetch(url)
+        .then(response=>response.json())
+        .then((data)=>{
+            data.forEach((item) =>{
+                showCountry(item);
+                
+            })
+        }).catch(error=>{
+            Error,error})
+    }
+  
+}
 
 
-// });
+function clearCard(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
